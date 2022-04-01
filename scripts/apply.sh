be5e0f16350a3cf2b413180dd57bb9ad3b2f0436
filou59 <p.git@flot.ovh>
2022-04-01 23:09:21 +0200

    Update apply.sh

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -9,6 +9,7 @@ JSLIBFILE="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
 
 pve_log_folder="/var/log/pve/tasks/"
 proxmoxlib="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
+
 # ---------------END OF ENVIRONNEMENT VARIABLES-----------------
 
 function pve_patch() {
@@ -25,20 +26,21 @@ function pve_patch() {
   
 	# (6.2-15 6.3-2 6.3-3 6.3-4 6.3-6 6.3-7 6.4-4 6.4-5 6.4-6 6.4-7 and up)
 	# Proxmox 7.1.7 and Up
-	if grep -q ".data.status.toLowerCase() == 'active') {" $proxmoxlib; then
-		echo "- Subscription Message already removed - Skipping"
-	else
-		if [ -d "$pve_log_folder" ]; then
-			echo "- Removing No Valid Subscription Message for PVE"
-			#sed -Ezi.bak "s/!== 'active'/== 'active'/" $proxmoxlib && echo "- Restarting proxy service" && systemctl restart pveproxy.service
-			sed -i.bak -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" ${proxmoxlib}
-		else 
-			echo "- Removing No Valid Subscription Message for PBS"
-			sed -Ezi.bak "s/!== 'active'/== 'active'/" $proxmoxlib && echo "- Restarting proxy service" && systemctl restart proxmox-backup-proxy.service
-		fi
+	#if grep -q ".data.status.toLowerCase() == 'active') {" $proxmoxlib; then
+	#	echo "- Subscription Message already removed - Skipping"
+	#else
+	if [ -d "$pve_log_folder" ]; then
+		echo "- Removing No Valid Subscription Message for PVE"
+		#sed -Ezi.bak "s/!== 'active'/== 'active'/" $proxmoxlib && echo "- Restarting proxy service" && systemctl restart pveproxy.service
+		#sed -i.bak -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" ${proxmoxlib}
+		sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" ${proxmoxlib} && echo"Found PopupMessage" && systemctl restart pveproxy.service
+	else 
+		echo "- Removing No Valid Subscription Message for PBS"
+		sed -Ezi.bak "s/!== 'active'/== 'active'/" $proxmoxlib && echo "- Restarting proxy service" && systemctl restart proxmox-backup-proxy.service
 	fi
+	#fi
 
-	systemctl restart pveproxy.service
+	#systemctl restart pveproxy.service
   
 	# pvedarkdiscord theme: courtesy of https://github.com/Weilbyte/PVEDiscordDark
 	#bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
