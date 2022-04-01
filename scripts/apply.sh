9a2d70ef8808d06073cb553ee4b04c02022e1b88
filou59 <p.git@flot.ovh>
2022-04-01 22:06:59 +0200

    Update apply.sh

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,16 +1,48 @@
 #!/usr/bin/env bash
 
+# -----------------ENVIRONNEMENT VARIABLES----------------------
 DEBIAN_CODENAME=`cat /etc/os-release | grep VERSION_CODENAME | cut -d "=" -f2`
 ENTERPRISE_REPO_LIST="/etc/apt/sources.list.d/pve-enterprise.list"
 FREE_REPO_LIST="/etc/apt/sources.list.d/pve.list"
 FREE_REPO_LINE="deb http://download.proxmox.com/debian/pve $DEBIAN_CODENAME pve-no-subscription"
+JSLIBFILE="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
+
+pve_log_folder="/var/log/pve/tasks/"
+proxmoxlib="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
+# ---------------END OF ENVIRONNEMENT VARIABLES-----------------
 
 function pve_patch() {
-  echo "- apply patch..."
-  echo $FREE_REPO_LINE > $FREE_REPO_LIST
-  [ -f $ENTERPRISE_REPO_LIST ] && mv $ENTERPRISE_REPO_LIST $ENTERPRISE_REPO_LIST~
-  sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
-  cp --backup /usr/share/pve-patch/images/* /usr/share/pve-manager/images/
+	echo "- apply patch..."
+	echo $FREE_REPO_LINE > $FREE_REPO_LIST
+	[ -f $ENTERPRISE_REPO_LIST ] && mv $ENTERPRISE_REPO_LIST $ENTERPRISE_REPO_LIST~
+  
+	# (6.1 and up)
+	#sed -i.backup "s/data.status !== 'Active'/false/g" ${proxmoxlib}
+	# (6.2-11 and up)
+	#sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.false/false/g" ${proxmoxlib}
+	# (6.2-12 and up)
+	#sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status \!== 'Active'/false/g" ${proxmoxlib}
+  
+	# (6.2-15 6.3-2 6.3-3 6.3-4 6.3-6 6.3-7 6.4-4 6.4-5 6.4-6 6.4-7 and up)
+	# Proxmox 7.1.7 and Up
+	if grep -q ".data.status.toLowerCase() == 'active') {" $proxmoxlib; then
+		echo "- Subscription Message already removed - Skipping"
+	else
+		if [ -d "$pve_log_folder" ]; then
+			echo "- Removing No Valid Subscription Message for PVE"
+			sed -Ezi.bak "s/!== 'active'/== 'active'/" $proxmoxlib && echo "- Restarting proxy service" && systemctl restart pveproxy.service
+			#sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" ${JSLIBFILE}
+		else 
+			echo "- Removing No Valid Subscription Message for PBS"
+			sed -Ezi.bak "s/!== 'active'/== 'active'/" $proxmoxlib && echo "- Restarting proxy service" && systemctl restart proxmox-backup-proxy.service
+		fi
+	fi
+
+	#systemctl restart pveproxy.service
+  
+	# pvedarkdiscord theme: courtesy of https://github.com/Weilbyte/PVEDiscordDark
+	#bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
+	#systemctl restart pveproxy.service
 }
 
 pve_patch
