f6a5cc6c8e49d7e1f772269520fd207390e2d74b
filou59 <p.git@flot.ovh>
2022-04-05 06:37:19 +0200

    Update apply.sh

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -30,10 +30,17 @@ function pve_patch() {
 	#	echo "- Subscription Message already removed - Skipping"
 	#else
 	if [ -d "$pve_log_folder" ]; then
-		echo "- Removing No Valid Subscription Message for PVE"
+		result=$( grep -B 1 'No valid sub' $proxmoxlib | grep -i 'void' )
+		#echo "Result" $result
+    if [ -n "$result" ]; then      
+      echo "- Removing No Valid Subscription Message Already Done"
+    else  
+      echo "- Removing No Valid Subscription Message for PVE"
+      sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('N   o valid sub)/void\(\{ \/\/\1/g" ${proxmoxlib} && systemctl restart pveproxy.service      
+    fi  
 		#sed -Ezi.bak "s/!== 'active'/== 'active'/" $proxmoxlib && echo "- Restarting proxy service" && systemctl restart pveproxy.service
 		#sed -i.bak -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" ${proxmoxlib}
-		sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" ${proxmoxlib} && echo "- Found Popup Message" && systemctl restart pveproxy.service
+		
 	else 
 		echo "- Removing No Valid Subscription Message for PBS"
 		sed -Ezi.bak "s/!== 'active'/== 'active'/" $proxmoxlib && echo "- Restarting proxy service" && systemctl restart proxmox-backup-proxy.service
